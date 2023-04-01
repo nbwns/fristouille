9f6644937acbc757975dac3c9a7f20be2434cfd1
stephanesdr <stadlerstephane@gmail.com>
2023-04-01 14:16:02 +0200

    style for filter dropDown component /Search page

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -2,14 +2,16 @@
 	<div class="dropdown">
 		<button @click.prevent="toggle" id="menu-button" type="button" class="dropdown-wrap navlink" aria-expanded="true"
 			aria-haspopup="true">
-			<div class="text-small">
-				{{ title }}
+			<div class="flex flex-row">
+				<div class="text-medium">
+					{{ title }}
+				</div>
 				<svg class="w-6 h-6 fill-white-300" stroke-width="3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-					aria-hidden="true">
-					<path fill-rule="evenodd"
-						d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-						clip-rule="evenodd" />
-				</svg>
+						aria-hidden="true">
+						<path fill-rule="evenodd"
+							d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+							clip-rule="evenodd" />
+					</svg>
 			</div>
 		</button>
 		<transition enter-active-class="transition ease-out duration-250" enter-class="opacity-0 scale-100 -translate-y-4"
