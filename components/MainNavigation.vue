22fe2b5dc722229d030821200e6e124888f5ec54
stephanesdr <stadlerstephane@gmail.com>
2023-04-06 20:52:01 +0200

    update page recette durable

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -1,18 +1,20 @@
 <template>
   <nav class="flex items-center justify-between flew-wrap p-4 bg-black-300 dark:bg-white-300">
-    <logo/>
-    <div class="hidden md:flex flex-grow md:w-auto pt-2 ">
-      <div class="flex flex-grow space-x-5">
-      <nav-link path="/">Accueil</nav-link>
-	  	<nav-link path="/recettes">Rechercher</nav-link>
-	  	<nav-link path="/recettes-de-base">Recettes de base</nav-link>
-	  	<nav-link path="/cuisine-durable">Cuisine durable</nav-link>
-      </div>
-      <div>
-        <a href="#"
-          class="navlink">
-          Connexion
-        </a>
+    <div class="layer__2xl w-full mx-auto">
+      <logo/>
+      <div class="hidden md:flex flex-grow md:w-auto pt-2 ">
+        <div class="flex flex-grow space-x-5">
+        <nav-link path="/">Accueil</nav-link>
+            <nav-link path="/recettes">Rechercher</nav-link>
+            <nav-link path="/recettes-de-base">Recettes de base</nav-link>
+            <nav-link path="/cuisine-durable">Cuisine durable</nav-link>
+        </div>
+        <div>
+          <a href="#"
+            class="navlink">
+            Connexion
+          </a>
+        </div>
       </div>
     </div>
   </nav>
