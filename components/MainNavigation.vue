582a702ee15a24b81273c2ce0c3e13f1651b901b
Stephane Stadler <stephanestadler@MacBook-Pro-de-Stephane.local>
2024-04-20 21:08:21 +0200

    UI amelioration homepage

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="flex items-center justify-between flew-wrap p-4 bg-black-300 dark:bg-white-300">
-    <div class="layer__xl w-full flex flex-row  md:flex-col justify-between mx-auto items-center md:items-start">
+    <div class="layer__xl w-full flex flex-row justify-between mx-auto items-center">
       <logo/>
       <div class="hidden md:flex flex-grow md:w-auto pt-2 ">
-        <div class="flex flex-grow space-x-5">
+        <div class="flex flex-grow space-x-5 justify-end">
 			<nuxt-link class="navlink" to="/" no-prefetch exact>Accueil</nuxt-link>
             <nuxt-link class="navlink" to="/recettes" no-prefetch>Recettes</nuxt-link>
             <nuxt-link class="navlink" to="/astuces" no-prefetch>Astuces</nuxt-link>
