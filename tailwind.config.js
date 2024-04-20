1c2f8efee289edd0bcaed34fb651cf76344458bd
Stephane Stadler <stephanestadler@MacBook-Pro-de-Stephane.local>
2024-04-20 21:32:34 +0200

    colors goes into extend theme and strong color add to darkmode navlink

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,54 +20,7 @@ module.exports = {
       'md': '0.75rem',
       'xs': '0.1rem',
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-
-      orange: {
-        100: "#FFD9C3",
-        200: "#FFA06A",
-        300: "#FC762B",
-      },
-
-      purple: {
-        100: "#D1B9DF",
-        200: "#B390C9",
-        300: "#734490",
-      },
-
-      green: {
-        100: "#B6D9C8",
-        200: "#50A77D",
-        300: "#0C7945",
-      },
-
-      red: {
-        100: "#F9C5C6",
-        200: "#E55F5F",
-        300: "#D03030",
-      },
-
-      pink: {
-        100: "#F4D1F4",
-        200: "#EB8DED",
-        300: "#D450D7",
-      },
-
-      black: {
-        50: "#6C5C6D",
-        100: "#423343",
-        200: "#2D1F2E",
-        300: "#190B1A",
-      },
-
-      white: {
-        100: "#F3E9F4",
-        200: "#FCF7FB",
-        300: "#FFFFFF",
-      },
-
-    },
+   
     fontSize: {
       '4xl': "3em",
       '3xl': '2.5em',
@@ -83,7 +36,56 @@ module.exports = {
         labil: ["Labil Grotesk", "sans-serif"],
         inter: ["Inter", "sans-serif"],
       },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+  
+        orange: {
+          100: "#FFD9C3",
+          200: "#FFA06A",
+          300: "#FC762B",
+        },
+  
+        purple: {
+          100: "#D1B9DF",
+          200: "#B390C9",
+          300: "#734490",
+        },
+  
+        green: {
+          100: "#B6D9C8",
+          200: "#50A77D",
+          300: "#0C7945",
+        },
+  
+        red: {
+          100: "#F9C5C6",
+          200: "#E55F5F",
+          300: "#D03030",
+        },
+  
+        pink: {
+          100: "#F4D1F4",
+          200: "#EB8DED",
+          300: "#D450D7",
+        },
+  
+        black: {
+          50: "#6C5C6D",
+          100: "#423343",
+          200: "#2D1F2E",
+          300: "#190B1A",
+        },
+  
+        white: {
+          100: "#F3E9F4",
+          200: "#FCF7FB",
+          300: "#FFFFFF",
+        },
+  
+      },
     },
+   
   },
   plugins: [],
 };
