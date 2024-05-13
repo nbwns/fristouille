bb03774e4f04c4e832cfe24e0ae5245702789052
Stephane Stadler <stephanestadler@MacBook-Pro-de-Stephane.local>
2024-05-13 14:59:18 +0200

    add minor change for index and layout vue mobile and desktop UI

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav class="flex items-center justify-between flew-wrap p-1 md:p-4 bg-black-300 dark:bg-white-300">
-    <div class="layer__xl w-full flex flex-row justify-between mx-auto items-center">
+  <nav class="flex items-center justify-between flew-wrap px-1 py-3 md:p-4 bg-black-300 dark:bg-white-300">
+    <div class="layer__xl w-full flex flex-row justify-between mx-auto items-center">      
       <logo/>
       <div class="hidden md:flex flex-grow md:w-auto pt-2 ">
-        <div class="flex flex-grow space-x-5 justify-end">
-			<nuxt-link class="navlink" to="/" no-prefetch exact>Accueil</nuxt-link>
+        <div class="flex flex-grow space-x-5 justify-end">			
+          <nuxt-link class="navlink" to="/" no-prefetch exact>Accueil</nuxt-link>
             <nuxt-link class="navlink" to="/recettes" no-prefetch>Recettes</nuxt-link>
             <nuxt-link class="navlink" to="/astuces" no-prefetch>Astuces</nuxt-link>
         </div>
