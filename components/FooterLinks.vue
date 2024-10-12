bd9307720bb1a3559f46bdd8ab16b0faa52d94f6
Stephane Stadler <stadlerstephane@gmail.com>
2024-10-13 01:31:50 +0200

    git add --all

--- components/FooterLinks.vue
+++ components/FooterLinks.vue
@@ -26,7 +26,7 @@
 							<eco-index-badge />
 							<dark-mode-toggle />
 						</div>
-						<p class="text-sm text-right pt-3 text-label">copyright ©fristouille {{ currentYear }} safdsa</p>
+						<p class="text-sm text-right pt-3 text-label">copyright ©fristouille {{ currentYear }}</p>
 					</div>
 				</div>
 			</div>
