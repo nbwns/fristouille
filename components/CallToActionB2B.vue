a4b48f44d37adededf70c877075520d7178f01f7
Nicolas Bauwens <nicolas.bauwens@gmail.com>
2026-06-26 08:51:44 +0200

    draft B2B CTA block

---
+++ components/CallToActionB2B.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-col w-full rounded-md bg-secondary group border border-primary-foreground/10 hover:border-primary-foreground/20 hover:bg-primary/80 transition-colors duration-300 overflow-hidden">
+  <div class="flex flex-col justify-between px-6 py-5 w-full">
+    <div class="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+      <div>
+        <h3 class="text-foreground text-lg leading-tight font-demi ligatures-none lowercase">
+          tu travailles dans une commune, une école ou une asso ?
+        </h3>
+        <p class="text-sm text-foreground/80 font-light pt-2">
+          J'anime des ateliers et formations en alimentation durable à Bruxelles — pour sensibiliser enfants, ados et adultes de manière ludique et pratique.
+        </p>
+      </div>
+      <a
+        href="/a-propos/services"
+        class="w-fit inline-flex h-8 px-3.5 tracking-wide items-center justify-center bg-secondary hover:bg-primary text-primary-foreground border border-primary-foreground/10 hover:border-primary-foreground/60 rounded whitespace-nowrap text-sm font-mono font-medium ring-offset-primary-foreground/10 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-300 dark:focus-visible:ring-purple-300 focus-visible:ring-offset-2 shrink-0"
+      >
+        Découvrir mes services →
+      </a>
+    </div>
+  </div>
+</div>
+</template>
